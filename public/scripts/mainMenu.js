3bc00c19392a308a2394ffdd9de4f57bb49aaa1b
JellyPumps <sarthakraibhartiya@gmail.com>
2024-11-10 12:44:35 +0400

    script for main menu scene

---
+++ public/scripts/mainMenu.js
@@ -0,0 +1,30 @@
+function createRoom() {
+    const name = document.getElementById('name').value;
+    const role = document.getElementById('role').value;
+    if (name) socket.emit('createRoom', { name, role });
+}
+
+function joinRoom() {
+    const roomCode = document.getElementById('room-code').value;
+    const name = document.getElementById('name').value;
+    const role = document.getElementById('role').value;
+    if (name && roomCode) {
+        socket.emit('joinRoom', { roomCode, name, role });
+    }
+}
+
+// Events
+document.addEventListener('roomCreated', (e) => {
+    currentRoom = e.detail;
+    roomJoined(e);
+});
+
+document.addEventListener('roomJoined', (e) => {
+    roomJoined(e);
+});
+
+function roomJoined(e) {
+    const roomCode = e.detail;
+    console.log('Joined room:', roomCode);
+    sceneManager.loadScene('lobby', { roomCode });
+}
